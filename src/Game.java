6661ddf0f7cf888256c661466c84c8517b1c9523
Enes Turkoglu <enes.turkoglu.tal@gmail.com>
2025-02-23 08:01:03 +0300

    Add Game class with basic JPanel setup

---
+++ src/Game.java
@@ -0,0 +1,36 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+public class Game extends JPanel implements KeyListener,ActionListener {
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+	
+	
+
+}
